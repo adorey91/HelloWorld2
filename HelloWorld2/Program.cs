1fd187e2d37e86f06ccd266aff5d10754b2e451d
Adrian Dorey <144232716+adorey91@users.noreply.github.com>
2023-09-12 15:00:27 -0300

    Initial commit

---
+++ HelloWorld2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); // displays "hello world"
+            Console.ReadKey(); // wait for user keypress
+
+            // end of program
+        }
+    }
+}
